dbf419b978259083c1d6c63ca1b5ddfd3b92b223
Unknown <tongnop2540@gmail.com>
2019-06-21 14:42:40 +0700

    compleate

--- modules/web/src/com/company/workflowexample/web/screens/contract/ContractEdit.java
+++ modules/web/src/com/company/workflowexample/web/screens/contract/ContractEdit.java
@@ -1,16 +1,5 @@
 package com.company.workflowexample.web.screens.contract;
 
-import com.haulmont.cuba.gui.screen.*;
-import com.company.workflowexample.entity.Contract;
-
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.haulmont.bpm.entity.ProcAttachment;
-import com.haulmont.bpm.gui.procactions.ProcActionsFrame;
-import com.haulmont.cuba.gui.app.core.file.FileDownloadHelper;
-import com.haulmont.cuba.gui.components.Table;
-
-import javax.inject.Inject;
-
 import com.haulmont.bpm.entity.ProcAttachment;
 import com.haulmont.bpm.gui.procactionsfragment.ProcActionsFragment;
 import com.haulmont.cuba.gui.app.core.file.FileDownloadHelper;
@@ -27,7 +16,7 @@ import javax.inject.Inject;
 @UiDescriptor("contract-edit.xml")
 @EditedEntityContainer("contractDc")
 public class ContractEdit extends StandardEditor<Contract> {
-    private static final String PROCESS_CODE = "contractApproval";
+    private static final String PROCESS_CODE = "contractApprovel";
 
     @Inject
     private CollectionLoader<ProcAttachment> procAttachmentsDl;
@@ -56,29 +45,3 @@ public class ContractEdit extends StandardEditor<Contract> {
         FileDownloadHelper.initGeneratedColumn(attachmentsTable, "file");
     }
 }
-//
-//@UiController("workflowexample_Contract.edit")
-//@UiDescriptor("contract-edit.xml")
-//@EditedEntityContainer("contractDc")
-//@LoadDataBeforeShow
-//public class  ContractEdit extends AbstractEditor<Contract> {
-//    private static final String PROCESS_CODE = "Contract Approvel";
-//
-//    @Inject
-//    private ProcActionsFrame procActionsFrame;
-//
-//    @Inject
-//    private Table<ProcAttachment> attachmentsTable;
-//
-//    @Override
-//    public void ready() {
-//        FileDownloadHelper.initGeneratedColumn(attachmentsTable, "file");
-//        initProcActionsFrame();
-//    }
-//
-//    private void initProcActionsFrame() {
-//        procActionsFrame.initializer()
-//                .standard()
-//                .init(PROCESS_CODE, getItem());
-//    }
-//}
